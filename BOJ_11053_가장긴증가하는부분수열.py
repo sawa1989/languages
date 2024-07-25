3e3b79657cddb9416bd5a88cdacdc79523d78454
literarysimilarity <97671354+sawa1989@users.noreply.github.com>
2024-07-25 13:36:45 +0900

    Create BOJ_11053_가장긴증가하는부분수열.py
    dp

---
+++ BOJ_11053_가장긴증가하는부분수열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+A = list(map(int, input().split()))
+
+dp = [1] * N
+
+for i in range(1, N):
+    for j in range(0,i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i],dp[j]+1 )
+            
+print(max(dp))
